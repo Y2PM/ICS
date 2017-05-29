faa930189a6948c2a2b33ad1a676f8d180956eb6
Joe <Joe@DESKTOP-GOO66JF>
2017-05-29 20:34:49 +0100

    Added unit tests

--- SearchClassLibrary/SearchClass.cs
+++ SearchClassLibrary/SearchClass.cs
@@ -9,7 +9,7 @@ namespace SearchClassLibrary
         List<string> CitiesList = new List<string>();
         int searchStringLength = 0;
         string longest = "";
-        int WordCount = 0;
+        //int WordCount = 0;
 
         public SearchClass(List<string> CitiesList_)
         {
@@ -18,7 +18,7 @@ namespace SearchClassLibrary
             //Get longest string to work out max length of city name
             foreach (string s in CitiesList)
             {
-                WordCount++;
+                //WordCount++;
                 if (s.Length > longest.Length)
                 {
                     longest = s;
