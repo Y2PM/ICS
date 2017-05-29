93535b84970469ca98ad06eddc49cb5fd11fd9af
Joe <Joe@DESKTOP-GOO66JF>
2017-05-29 21:04:20 +0100

    Finished requirements tests

--- SearchClassLibrary/GetCitiesClass.cs
+++ SearchClassLibrary/GetCitiesClass.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 
 namespace SearchClassLibrary
@@ -11,7 +10,7 @@ namespace SearchClassLibrary
             StreamReader sr = new StreamReader(Path);
             List<string> CitiesList = new List<string>();
 
-            String line = "";
+            string line = "";
             while ((line = sr.ReadLine()) != null)
             {
                 CitiesList.Add(line);
