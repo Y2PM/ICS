09cf17aee616a01e31b4fc66d0c82559f66aa57b
Joe <Joe@DESKTOP-GOO66JF>
2017-05-29 22:46:43 +0100

    Handled file not found exception

--- InternationalCitySearch.Project/Program.cs
+++ InternationalCitySearch.Project/Program.cs
@@ -15,11 +15,23 @@ namespace InternationalCitySearch.Project
             //1049939 Word list.txt
 
             GetCitiesClass GetCitiesObject = new GetCitiesClass();
-            List<string> CitiesList = GetCitiesObject.GetCitiesMethod("C://Users//Joe//Source//Repos//ICS//1049939 Word list.txt");
-            SearchClass SearchClassObject = new SearchClass(CitiesList);
+            List<string> CitiesList = new List<string>();
             string searchTearm = "";
             string continueQ = "Y";
 
+            try
+            {
+                CitiesList = GetCitiesObject.GetCitiesMethod("C://Users//Joe//Source//Repos//ICS//1049939 Word list.txt");
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine("Ensure correct path to search list is in source code.");
+                Console.WriteLine(e);
+                Console.ReadLine();
+                continueQ = "N";
+            }
+
+            SearchClass SearchClassObject = new SearchClass(CitiesList);//Instantiated outside of loop to increase user experience speed.
             while (continueQ.ToUpper() == "Y" || continueQ.ToUpper() == "YES")
             {
                 Console.WriteLine("Please enter search term (case sensitive): ");
